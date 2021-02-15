34571b2f75171f5475003958ff68b553a44e8ee4
Paul Sims <paulqsims@users.noreply.github.com>
2021-02-15 10:10:57 -0500

    Add answers for 4

--- sqlzoo/Self join.sql
+++ sqlzoo/Self join.sql
@@ -58,4 +58,21 @@ WHERE route.num = '4' AND route.company = 'LRT'
 -- Routes and stops
 -------------------
 
--- 4. 
+-- 4. The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2. Add a HAVING clause to restrict the output to these two routes.
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+
+-- Approach:
+-- 1. Add Having to group results by count column = 2
+
+-- Answer:
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2
+
+-- Notes:
+--> Remember, you can put aggregate functions in WHERE and HAVING statements, but will have to include them in the SELECT statement as well
