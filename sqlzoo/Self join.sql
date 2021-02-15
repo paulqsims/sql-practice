36223f82974ac3b6928207137d2c50eae55012a0
Paul Sims <paulqsims@users.noreply.github.com>
2021-02-15 09:38:13 -0500

    Add self-join section

---
+++ sqlzoo/Self join.sql
@@ -0,0 +1,19 @@
+-- SQLzoo: Self join
+-- Link: https://sqlzoo.net/wiki/Self_join
+-- Tables: stops, route
+
+
+-- 1. How many stops are in the database.
+
+-- Approach:
+-- 1. 
+
+-- Answer: 
+
+
+
+-- Notes:
+--> 
+
+
+-- 2. 
