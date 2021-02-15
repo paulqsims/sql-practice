98d2bbc540ddf54a4870824b932366afb5118d60
Paul Sims <paulqsims@users.noreply.github.com>
2021-02-15 09:57:44 -0500

    Add answer to 1

--- sqlzoo/Self join.sql
+++ sqlzoo/Self join.sql
@@ -1,19 +1,23 @@
 -- SQLzoo: Self join
 -- Link: https://sqlzoo.net/wiki/Self_join
--- Tables: stops, route
+-- Tables: Edinburgh Buses - stops, route
 
 
 -- 1. How many stops are in the database.
 
+-- Questions:
+-- 1. Unique or total number of stops?
+
 -- Approach:
--- 1. 
+-- 1. Count number of unique stops overall
 
 -- Answer: 
 
-
+SELECT COUNT(DISTINCT stop) as num_stops
+FROM route
 
 -- Notes:
---> 
-
+--> No GROUP BY since count of entire table
+--> They wanted unique stops even though that wasn't clear in the instructions
 
 -- 2. 
