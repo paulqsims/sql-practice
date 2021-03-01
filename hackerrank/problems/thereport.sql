6d19d493a6869223b8a9d3bbdc2ce93a1dec85f4
Paul Sims <paulqsims@users.noreply.github.com>
2021-03-01 11:52:24 -0500

    Add url

--- hackerrank/problems/thereport.sql
+++ hackerrank/problems/thereport.sql
@@ -1,4 +1,5 @@
 -- The Report
+-- https://www.hackerrank.com/challenges/the-report/problem
 
 -- Prompt:
 -- You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
