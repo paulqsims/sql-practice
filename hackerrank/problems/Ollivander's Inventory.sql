85d2db608e9074213120ae4fdb7b1aed4d4c35c6
Paul Sims <paulqsims@users.noreply.github.com>
2021-03-05 11:16:15 -0500

    Partial answer

--- hackerrank/problems/Ollivander's Inventory.sql
+++ hackerrank/problems/Ollivander's Inventory.sql
@@ -2,22 +2,44 @@
 -- https://www.hackerrank.com/challenges/harry-potter-and-wands
 
 -- Prompt:
--- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+-- Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+
+-- Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+
+
 
 -- Approach:
 
--- End table: HackerID | Name
--- Filter by full score, >1 challenge
--- Get full score from difficulty table
--- Calculate challenges > 1 by hacker and full score
--- ORDER tot num challenges that have full score
--- Ascending hacker id 
+-- End table: id | age | coins_needed | power
+-- Order by Power Desc, age DESC
+-- JOIN wands and wands property on code to get age and not evil wands
+-- Find the min coins grouped by wand 
 
 -- Subquery
--- 1. Get counts of challenges > 1 by hacker 
--- 2. Filter by full scores only
+-- 1. 
+-- 2. 
+
 -- Parent query
--- 3. Filter by counts > 1
--- 4. Order by counts desc, ids ascending
+-- 3. 
+-- 4. 
+
+-- Query
+-- 1. Join wands and wand property on code
+-- 2. Conditional on is_evil = 0
+-- 3. Get the min number of coins needed
+-- 4. Order 
+-- 5. Select
 
 -- Answer:
+
+-- DID NOT FINISH, question is unclear 
+
+SELECT id, age, 
+    MIN(coins_needed) OVER(PARTITION BY code, age, power) AS min_cost,
+       power
+FROM (
+    SELECT id, age, coins_needed, power, code
+    FROM Wands w JOIN Wands_Property wp ON w.code=wp.code AND 
+        is_evil = 0
+    ) AS t
+ORDER BY power DESC, age DESC
